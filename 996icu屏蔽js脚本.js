104e4b6a65385219e7b0ca080d8cbf93ee868cef
庄铭宇 <superzmy@qq.com>
2019-03-31 11:52:15 +0800

    Update 996icu屏蔽js脚本.js

--- 996icu屏蔽js脚本.js
+++ 996icu屏蔽js脚本.js
@@ -1,5 +1,13 @@
-for(var x of document.getElementsByClassName("body"))
+<script> for(var x of document.getElementsByClassName("body"))
 {
 	if (x.innerHTML.indexOf("script") >= 0)
 		x.innerHTML = "";
 }
+</script>
+
+&lt;script&gt; for(var x of document.getElementsByClassName(&quot;body&quot;))
+{
+	if (x.innerHTML.indexOf(&quot;script&quot;) &gt;= 0)
+		x.innerHTML = &quot;&quot;;
+}
+&lt;script&gt;
